7ae4c64730a695cd0dbd7c22cb2f7a71032735f7
Catarina Bento <caf.bneto@campus.fct.unl.pt>
2022-11-20 23:04:22 +0000

    First TopBar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import {HomeInventory} from "./components/HomeInventory/HomeInventory";
+import {TopBar} from "./components/TopBar/TopBar";
 
 function App() {
   return (
     <div className="App">
+        <TopBar/>
         <HomeInventory/>
     </div>
   );
