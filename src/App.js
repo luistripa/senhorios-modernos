756d4c7301a355454439b7fbcc87a9c0c9206f9e
Catarina Bento <caf.bneto@campus.fct.unl.pt>
2022-11-21 18:22:57 +0000

    Added HomeInventory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {TopBarAfterLogin} from "./components/TopBar/TopBarAfterLogin";
 import {TopBarBeforeLogin} from "./components/TopBar/TopBarBeforeLogin";
+import {HomeInventory} from "./components/HomeInventory/HomeInventory";
 
 function App() {
   return (
     <div className="App">
         <TopBarAfterLogin/>
         <TopBarBeforeLogin/>
+        <HomeInventory/>
     </div>
   );
 }
