a37c83562ba4d9b38bce5f3bec950016b5e22408
luistripa <luistripa@me.com>
2022-11-23 13:34:00 +0000

    Fixed dependency problems... again

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import LoginAndRegister from "./components/LoginAndRegister/LoginAndRegister";
-import React, { Component }  from 'react';
+import React from 'react';
 import {TopBarAfterLogin} from "./components/TopBar/TopBarAfterLogin";
 import {TopBarBeforeLogin} from "./components/TopBar/TopBarBeforeLogin";
 import {HomeInventory} from "./components/HomeInventory/HomeInventory";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {MyCalendar} from "./components/MyCalendar/MyCalendar";
+import MyHousesPage from "./components/MyHousesPage/MyHousesPage";
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
 
         <BrowserRouter>
             <Routes>
-                <Route path={"/mycalendar"} element={<MyCalendar/>}/>
+                <Route path={"/my-calendar"} element={<MyCalendar/>}/>
                 <Route path="/login-and-register" element={<LoginAndRegister />} />
                 <Route path={"/my-houses"} element={<MyHousesPage/>}/>
             </Routes>
