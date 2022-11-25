d0284a1c037195aa338391a0b9663f89db1169f1
catarinabento <caf.bento@campus.fct.unl.pt>
2022-11-25 22:19:13 +0000

    Geral refactoring: strikethrough not working properly

--- src/components/TODOList/TODOList.js
+++ src/components/TODOList/TODOList.js
@@ -9,18 +9,18 @@ import {
     ListItemIcon,
     ListItemText,
     Input,
-    Divider,
+    Box
 } from '@mui/material';
-import {Delete, Add} from '@mui/icons-material';
+import {Delete, Check, AddBox} from '@mui/icons-material';
 import {useEffect, useState} from "react";
 
-export function TODOList(props){
+export function TODOList(){
 
     const [todoList, setTodoList] = useState([]);
 
-    const [doneList, setDoneList] = useState([]);
-
     const [inputText, setInputText] = useState("");
+
+    const [addItem, setAddItem] = useState(false);
 
     useEffect(() => {
         setTodoList([
@@ -38,59 +38,45 @@ export function TODOList(props){
                 id: 2,
                 name: "Fix pipe",
                 isChecked: false
-            }
-        ]);
-    }, []);
-
-    useEffect(() => {
-        setDoneList([
+            },
             {
-                id: 0,
+                id: 3,
                 name: "Do laundry",
                 isChecked: true
             },
             {
-                id: 1,
+                id: 4,
                 name: "Buy washing machine",
                 isChecked: true
             }
         ]);
     }, []);
 
+    const handleInput = e => {
+        setInputText(e.target.value);
+    }
 
-    const handleToggle = (value) => {
-        let newTodoList = [];
-        let i = 0;
-        todoList.forEach(elem => {
-            if (i === value){
-                elem.isChecked = !elem.isChecked;
-                /*if(elem.isChecked){
-                    let newDoneList = [...doneList]
-                    newDoneList.push(elem);
-                    setDoneList(newDoneList);
-                }*/
-            }
-            /*else{
+    const handleKeyDown = e => {
+        if(e.key === 'Enter'){
+            handleAdd(e.target.value)
+        }
+    }
 
-            }*/
-            newTodoList.push(elem);
-            i++;
-        })
+    const handleAdd = (string) => {
+        let newTodoList = [...todoList];
+        newTodoList.push({
+            id: todoList.length,
+            name: string,
+            isChecked: false
+        });
         setTodoList(newTodoList);
-    };
+        setInputText("");
+        setAddItem(false);
+    }
 
-    const handleToggleDone = (value) => {
-        let newDoneList = [];
-        let i = 0;
-        doneList.forEach(elem => {
-            if (i === value){
-                elem.isChecked = !elem.isChecked;
-            }
-            newDoneList.push(elem);
-            i++;
-        })
-        setDoneList(newDoneList);
-    };
+    const handleAddButton = () => {
+        setAddItem(!addItem);
+    }
 
     const handleDelete= (value) => {
         let newTodoList = [];
@@ -103,115 +89,77 @@ export function TODOList(props){
         setTodoList(newTodoList);
     }
 
-    const handleDeleteDone= (value) => {
-        let newDoneList = [];
+    const handleToggle = (e, value) => {
+        let newTodoList = [];
         let i = 0;
-        doneList.forEach(elem => {
-            if (i !== value)
-                newDoneList.push(elem);
+        todoList.forEach(elem => {
+            if (i === value) {
+                elem.isChecked = !elem.isChecked;
+                if(elem.isChecked){
+                    e.target.style.setProperty('text-decoration', 'line-through');
+                }
+                else{
+                    e.target.style.removeProperty('text-decoration');
+                }
+            }
+            newTodoList.push(elem);
             i++;
         })
-        setDoneList(newDoneList);
-    }
-
-    const handleAdd = (string) => {
-        let newTodoList = [...todoList];
-        newTodoList.push({
-            id: listIds().length,
-            name: string,
-            isChecked: false
-        });
         setTodoList(newTodoList);
-        setInputText("");
-    }
+    };
 
-    const handleInput = e=> {
-        setInputText(e.target.value);
-    }
-
-    const listIds = () =>{
-        let ids = [];
-        todoList.forEach(elem => {
-            ids.push(elem.id)
-        })
-        return ids;
-    }
-
-    const listDoneIds = () =>{
-        let ids = [];
-        doneList.forEach(elem => {
-            ids.push(elem.id)
-        })
-        return ids;
-    }
-
-    return (
-        <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
-            {listIds().map((value) => {
-                const labelId = `checkbox-list-label-${value}`;
-
-                return (
-                    <ListItem
-                        key={value}
-                        secondaryAction={
-                            <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(value)}>
-                                <Delete/>
-                            </IconButton>
-                        }
-                        disablePadding
-                    >
-                        <ListItemButton role={undefined} onClick={() => handleToggle(value)} dense>
-                            <ListItemIcon>
-                                <Checkbox
-                                    edge="start"
-                                    checked={todoList[value].isChecked !== false}
-                                    inputProps={{'aria-labelledby': labelId}}
-                                />
-                            </ListItemIcon>
-                            <ListItemText id={labelId} primary={todoList[value].name} />
-                        </ListItemButton>
-                    </ListItem>
-                );
-            })}
-            <ListItem
-                secondaryAction={
-                    <IconButton edge="end" aria-label="add">
-                        <Add onClick={() => handleAdd(inputText)}/>
-                    </IconButton>
-            }
-            >
-                <Input value={inputText} onChange={handleInput}
-                       sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}
-                       placeholder="Type new item "
+    return(
+        <Box sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+            <List>
+                <ListItem
+                    secondaryAction={
+                        <IconButton edge="end" aria-label="addButton" onClick={() => handleAddButton()}>
+                            <AddBox/>
+                        </IconButton>
+                    }
                 />
-            </ListItem>
-            <Divider/>
-            {listDoneIds().map((value) => {
-                const labelId = `checkbox-list-label-${value}`;
-
-                return (
+                {addItem &&
                     <ListItem
-                        key={value}
-                        secondaryAction={
-                            <IconButton edge="end" aria-label="delete" onClick={() => handleDeleteDone(value)}>
-                                <Delete/>
-                            </IconButton>
-                        }
-                        disablePadding
+                    secondaryAction={
+                        <IconButton edge="end" aria-label="add">
+                            <Check onClick={() => handleAdd(inputText)}/>
+                        </IconButton>
+                    }
                     >
-                        <ListItemButton role={undefined} onClick={() => handleToggleDone(value)} dense>
-                            <ListItemIcon>
-                                <Checkbox
-                                    edge="start"
-                                    checked={doneList[value].isChecked !== false}
-                                    inputProps={{'aria-labelledby': labelId}}
-                                />
-                            </ListItemIcon>
-                            <ListItemText id={labelId} primary={doneList[value].name} />
-                        </ListItemButton>
+                        <Input value={inputText} onChange={handleInput} onKeyDown={handleKeyDown}
+                               sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}
+                               placeholder="Type new item "
+                        />
                     </ListItem>
-                );
-            })}
-        </List>
+                }
+                {[...Array(todoList.length).keys()].map((value) => {
+                    const labelId = `checkbox-list-label-${value}`;
+
+                    return (
+                        <ListItem
+                            key={value}
+                            secondaryAction={
+                                <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(value)}>
+                                    <Delete/>
+                                </IconButton>
+                            }
+                            disablePadding
+                        >
+                            <ListItemButton role={undefined} onClick={e => handleToggle(e, value)} dense>
+                                <ListItemIcon>
+                                    <Checkbox
+                                        edge="start"
+                                        checked={todoList[value].isChecked !== false}
+                                        inputProps={{'aria-labelledby': labelId}}
+                                    />
+                                </ListItemIcon>
+                                <ListItemText id={labelId} primary={todoList[value].name}/>
+                            </ListItemButton>
+                        </ListItem>
+                    );
+                })}
+            </List>
+        </Box>
     );
+
 }
