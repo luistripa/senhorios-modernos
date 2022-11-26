fccddd9a9ccc8708c153dcda27eb9f6580400e07
catarinabento <caf.bento@campus.fct.unl.pt>
2022-11-26 16:09:11 +0000

    App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
+import LoginAndRegister from "./components/LoginAndRegister/LoginAndRegister";
 import React from 'react';
-import {TODOList} from "./components/TODOList/TODOList";
+import {TopBarAfterLogin} from "./components/TopBar/TopBarAfterLogin";
+import {TopBarBeforeLogin} from "./components/TopBar/TopBarBeforeLogin";
+import {HomeInventory} from "./components/HomeInventory/HomeInventory";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {MyCalendar} from "./components/MyCalendar/MyCalendar";
+import MyHousesPage from "./components/MyHousesPage/MyHousesPage";
 
 
 function App() {
 
   return (
     <div className="App">
-        <TODOList/>
+        <TopBarAfterLogin/>
+        <TopBarBeforeLogin/>
+
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/my-calendar"} element={<MyCalendar/>}/>
+                <Route path="/login-and-register" element={<LoginAndRegister />} />
+                <Route path={"/my-houses"} element={<MyHousesPage/>}/>
+            </Routes>
+        </BrowserRouter>
+        
+        <HomeInventory/>
     </div>
   )
 }
