6246e366bcca4dcdff828f9fdeca13c4a173faee
Raquel <rc.melo@campus.fct.unl.pt>
2022-11-26 19:00:54 +0000

    Login

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 
 export default axios.create({
-    baseURL: 'http://localhost:3000/'
+    baseURL: 'http://evt.dyndns-ip.com:1243/'
 })
 
--- src/components/LoginAndRegister/LoginAndRegister.js
+++ src/components/LoginAndRegister/LoginAndRegister.js
@@ -1,40 +1,57 @@
 import './LoginAndRegister.css'
 import {useEffect, useState} from "react";
+import {useSearchParams} from "react-router-dom";
+import API from "../../api"
 
 const LoginAndRegister = () => {
 
     const [login, setLogin] = useState(true);
+    const [searchParams, setSearchParams] = useSearchParams();
+    useEffect( () => {
+        if(searchParams.get('register') != null)
+            setLogin(false);
+        }, []);
 
     function toggleLogin() {
         setLogin(!login);
     }
 
+    //Login
     const submitHandler = (e) => {
         e.preventDefault();
+
+        if(login) {
+            let user = {username: e.target.username.value, password: e.target.password.value};
+            API.post("/users/auth", user).then(response => {
+                let token = response.headers.get('Authorization');
+                sessionStorage.setItem('token', token);
+            } );
+        }
+        else{
+
+        }
     }
 
     return(
             <div className="main-container_LAR">
                 <div style={{transform: `translate(${login ? 0 : 95}%, 0px)`, zIndex:1}} className="form-container_LAR">
-                    <form className="form">
+                    <form className="form" onSubmit={submitHandler}>
                         {
                             login ? <>
                                 <h2 className="form-title">Login</h2>
+                                <input name="username" required="required" type="text" className="form-input" placeholder="Username"/>
+                                <input name="password" required="required" type="password" className="form-input" placeholder="Password"/>
+                                <a className="form-link" href="#"><b>Forgot your Password?</b></a>
                             </> : <>
                                 <h2 className="form-title">Create Account</h2>
-                                <input required="required" type="text" className="form-input" placeholder="Username"/>
-                                <input required="required" type="text" className="form-input" placeholder="Name"/>
+                                <input name="username" required="required" type="text" className="form-input" placeholder="Username"/>
+                                <input name="name" required="required" type="text" className="form-input" placeholder="Name"/>
+                                <input name="email" required="required" type="email" className="form-input" placeholder="E-mail"/>
+                                <input name="password" required="required" type="password" className="form-input" placeholder="Password"/>
+                                <input name="conf-password" required="required" type="password" className="form-input" placeholder="Confirm password"/>
                             </>
                         }
-                        <input required="required" type="email" className="form-input" placeholder="E-mail"/>
-                        <input required="required" type="password" className="form-input" placeholder="Password"/>
-                        {
-                            login ? <></> : <>
-                                <input required="required" type="password" className="form-input" placeholder="Confirm password"/>
-                                <a className="form-link" href="#"><b>Forgot your Password?</b></a>
-                            </>
-                        }
-                        <button className="form-button"> {login ? 'Login' : 'Sign Now'} </button>
+                        <button type="submit" className="form-button"> {login ? 'Login' : 'Sign Now'} </button>
                     </form>
                 </div>
 
