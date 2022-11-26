603ab1ffca07a6027a43a5a93424080246c563b1
Inês Costa <imma.costa@campus.fct.unl.pt>
2022-11-26 21:32:09 +0000

    Started Dashboard Info

--- src/components/MyHousesPage/MyHousesPage.js
+++ src/components/MyHousesPage/MyHousesPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {EventsList} from "./EventsList/EventsList";
 import {HousesList} from "./HousesList/HousesList";
 import {MyHousesHeader} from "./Header/MyHousesHeader";
-import {RentalsInfo} from "./RentalsInfo/RentalsInfo";
+import {DashboardInfo} from "./DashboardInfo/DashboardInfo";
 import './MyHousesPage.css';
 
 export default function MyHousesPage() {
@@ -11,14 +11,12 @@ export default function MyHousesPage() {
         <div>
             <MyHousesHeader/>
             <div className={'containerFirstPart'}>
-                <div className={'containerRentalInfo'}>
-                    <RentalsInfo/>
-                </div>
-                <div className={'containerHousesList'}>
-                    <HousesList/>
-                </div>
+                 <DashboardInfo/>
             </div>
-            <div className={'containerSecondPart'}>
+            <div className={'containerHousesList'}>
+                <HousesList/>
+            </div>
+            <div className={'containerEventsList'}>
                 <EventsList/>
             </div>
         </div>
