a278239e88196efa7461c5139c34d60fadd05644
Luis Tripa <l.tripa@campus.fct.unl.pt>
2022-11-28 15:27:53 +0000

    Home page fixes

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -13,6 +13,7 @@ import raquelImage from "../../static/raquel.jpg";
 import {TopBar} from "../TopBar/TopBar";
 import introImage from "../../static/frontpage-intro.png";
 import {Footer} from "./Footer";
+import {Grid} from "@mui/material";
 
 export const Homepage = () => {
 
@@ -38,139 +39,146 @@ export const Homepage = () => {
         return (
             <>
                 <div className={'introSection'} style={{display: "flex", flexDirection: "row"}}>
-                    <div style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: 'center',
-                        width: '60%',
-                        margin: '0 4% 0 4%'
-                    }}>
-                        <img src={'telhado-casa.png'}
-                             style={{width: "114px", position: "absolute", top: "72%", left: "2%"}}/>
-                        <div className={'titles about'}>
-                            <p style={{
-                                color: "#5D4DAA",
-                                fontWeight: '700',
-                                fontSize: '30px',
-                                margin: '0',
-                            }}> FINALLY,</p>
-                            <p style={{color: "#000000", fontWeight: '400', fontSize: '24px'}}> A WAY TO ORGANIZE YOUR
-                                HOUSES!</p>
-                        </div>
-                        <br/>
-                        <p>
-                            OneHome website is a place for you to gather all your rented houses information in just one
-                            place!
-                        </p>
-                        <p>
-                            This website will help you manage all your tasks, stock and dates!
-                        </p>
-                        <p>
-                            All in a easy and efficient website.
-                        </p>
-                        <p>
-                            Create an account and start organizing right now!
-                        </p>
 
-                    </div>
-                    <img src={introImage} width={'40%'}></img>
-                </div>
-                <div className={'servicesSection'} style={{display: 'flex', flexDirection: 'column', marginTop: '5%'}}>
-                    <div className={'titles services'} style={{display: 'flex', flexDirection: 'column'}}>
-                        <p className={'firstTitle'}>
-                            WHAT WE DO?
-                        </p>
-                        <p className={'secondTitle'}>
-                            OUR SERVICES
-                        </p>
-                    </div>
-                    <div className={'services'}>
-                        <div className={'service s1'}>
-                            <img src={'to-do-lists.png'}/>
-                            <div className={'description s1'}>
-                                <p className={'firstDescription'}>
-                                    To-do Lists
+                    <Grid container xs={12}>
+                        <Grid item xs={6} style={{display: "flex", alignItems: "center"}}>
+                            <div style={{margin: "0 10%"}}>
+                                <img src={'telhado-casa.png'}
+                                     style={{width: "114px", transform: "translate(0, 20%)"}}/>
+                                <div className={'titles about'}>
+                                    <p style={{
+                                        color: "#5D4DAA",
+                                        fontWeight: '700',
+                                        fontSize: '30px',
+                                        margin: '0',
+                                    }}> FINALLY,</p>
+                                    <p style={{color: "#000000", fontWeight: '400', fontSize: '24px'}}> A WAY TO ORGANIZE YOUR
+                                        HOUSES!</p>
+                                </div>
+                                <br/>
+                                <p>
+                                    OneHome website is a place for you to gather all your rented houses information in just one
+                                    place!
                                 </p>
-                                <p className={'secondDescription'}>
-                                    For each one of your houses!
+                                <p>
+                                    This website will help you manage all your tasks, stock and dates!
                                 </p>
-                            </div>
-                        </div>
-                        <div className={'service s2'}>
-                            <img src={'to-do-lists.png'}/>
-                            <div className={'description s2'}>
-                                <p className={'firstDescription'}>
-                                    Your Today Events
+                                <p>
+                                    All in a easy and efficient website.
                                 </p>
-                                <p className={'secondDescription'}>
-                                    Everything you have to do before you rest!
+                                <p>
+                                    Create an account and start organizing right now!
                                 </p>
                             </div>
-                        </div>
-                        <div className={'service s3'}>
-                            <img src={'to-do-lists.png'}/>
-                            <div className={'description s3'}>
-                                <p className={'firstDescription'}>
-                                    All Your Events
-                                </p>
-                                <p className={'secondDescription'}>
-                                    All your houses' events together in one timeline
-                                </p>
-                            </div>
-                        </div>
-                        <div className={'service s4'}>
-                            <img src={'to-do-lists.png'}/>
-                            <div className={'description s4'}>
-                                <p className={'firstDescription'}>
-                                    Your House Inventory
-                                </p>
-                                <p className={'secondDescription'}>
-                                    All your houses' events together in one timeline
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
-                <div className={'teamSection'}>
-                    <div className={'titles team'} style={{display: 'flex', flexDirection: 'column'}}>
-                        <p className={'firstTitle'}>
-                            WHO ARE WE?
-                        </p>
-                        <p className={'secondTitle'}>
-                            OUR TEAM
-                        </p>
-                    </div>
-                    <div className={'team-members'}>
-                        <div className={'members firstrow'} style={{display: 'flex', flexDirection: 'row'}}>
-                            <div className={'member m1'}>
-                                <img className={'img-member'} src={catarinaImage}/>
-                                <div className="info-member">
-                                    <div className="name-member">Catarina Bento</div>
+                        </Grid>
+                        <Grid item xs={6} sx={{display: "flex", alignItems: "center"}}>
+                            <img src={introImage} width={'100%'}></img>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <div className={'titles services'} style={{display: 'flex', flexDirection: 'column'}}>
+                                <p className={'firstTitle'}>
+                                    WHAT WE DO?
+                                </p>
+                                <p className={'secondTitle'}>
+                                    OUR SERVICES
+                                </p>
+                            </div>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <div className={'services'}>
+                                <div className={'service s1'}>
+                                    <img src={'to-do-lists.png'}/>
+                                    <div className={'description s1'}>
+                                        <p className={'firstDescription'}>
+                                            To-do Lists
+                                        </p>
+                                        <p className={'secondDescription'}>
+                                            For each one of your houses!
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className={'service s2'}>
+                                    <img src={'to-do-lists.png'}/>
+                                    <div className={'description s2'}>
+                                        <p className={'firstDescription'}>
+                                            Your Today Events
+                                        </p>
+                                        <p className={'secondDescription'}>
+                                            Everything you have to do before you rest!
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className={'service s3'}>
+                                    <img src={'to-do-lists.png'}/>
+                                    <div className={'description s3'}>
+                                        <p className={'firstDescription'}>
+                                            All Your Events
+                                        </p>
+                                        <p className={'secondDescription'}>
+                                            All your houses' events together in one timeline
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className={'service s4'}>
+                                    <img src={'to-do-lists.png'}/>
+                                    <div className={'description s4'}>
+                                        <p className={'firstDescription'}>
+                                            Your House Inventory
+                                        </p>
+                                        <p className={'secondDescription'}>
+                                            All your houses' events together in one timeline
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                            <div className={'member m2'}>
-                                <img className={'img-member'} src={inesImage}/>
-                                <div className="info-member">
-                                    <div className="name-member">Inês Costa</div>
+                        </Grid>
+
+                        <Grid item xs={12} style={{marginTop: "5%"}}>
+                            <div className={'titles team'} style={{display: 'flex', flexDirection: 'column'}}>
+                                <p className={'firstTitle'}>
+                                    WHO ARE WE?
+                                </p>
+                                <p className={'secondTitle'}>
+                                    OUR TEAM
+                                </p>
+                            </div>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <div className={'team-members'}>
+                                <div className={'members firstrow'} style={{display: 'flex', flexDirection: 'row'}}>
+                                    <div className={'member m1'}>
+                                        <img className={'img-member'} src={catarinaImage}/>
+                                        <div className="info-member">
+                                            <div className="name-member">Catarina Bento</div>
+                                        </div>
+                                    </div>
+                                    <div className={'member m2'}>
+                                        <img className={'img-member'} src={inesImage}/>
+                                        <div className="info-member">
+                                            <div className="name-member">Inês Costa</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={'members secondrow'} style={{display: 'flex', flexDirection: 'row'}}>
+                                    <div className={'member m3'}>
+                                        <img className={'img-member'} src={luisImage}/>
+                                        <div className="info-member">
+                                            <div className="name-member">Luís Tripa</div>
+                                        </div>
+                                    </div>
+                                    <div className={'member m4'}>
+                                        <img className={'img-member'} src={raquelImage}/>
+                                        <div className="info-member">
+                                            <div className="name-member">Raquel Melo</div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className={'members secondrow'} style={{display: 'flex', flexDirection: 'row'}}>
-                            <div className={'member m3'}>
-                                <img className={'img-member'} src={luisImage}/>
-                                <div className="info-member">
-                                    <div className="name-member">Luís Tripa</div>
-                                </div>
-                            </div>
-                            <div className={'member m4'}>
-                                <img className={'img-member'} src={raquelImage}/>
-                                <div className="info-member">
-                                    <div className="name-member">Raquel Melo</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        </Grid>
+                    </Grid>
                 </div>
                 <Footer/>
             </>
